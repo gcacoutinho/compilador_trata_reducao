0b3a63441b72f79cee8a3229d9065b8b5ccc6905
Denilson Rodrigues <jundenilson@gmail.com>
2019-11-22 20:24:06 -0300

    Faz mais reduções

--- src/trata_reducao.c
+++ src/trata_reducao.c
@@ -7,6 +7,8 @@ void trataReducao()
 			break;
 		case r1:
 			pop(1);
+			d[0].atributo = d[1].atributo;
+			// Ver se tem push
 			break;
 		case r2:
 			pop(3);
@@ -14,58 +16,105 @@ void trataReducao()
 			gera(STO, d[3].atributo, d[1].atributo);
 			break;
 		case r3:
+			// IF
 			pop(10);
 			break;
 		case r4:
+			// While
 			pop(8);
 			break;
 		case r5:
 			pop(3);
+			d[0].atributo = d[2].atributo;
+			d[0].simb = B;
+			d[0].estado = tabEstado[estado][B];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r6:
 			pop(1);
+			d[0].atributo = d[1].atributo;
+			d[0].simb = B;
+			d[0].estado = tabEstado[estado][B];
 			push(d[0]);
 			break;
-		case r7:
+		case r7:	
 			pop(3);
+			d[0].atributo = d[1].atributo;
+			d[0].simb = L;
+			d[0].estado = tabEstado[estado][L];
+			push(d[0]);
 			break;
 		case r8:
 			pop(1);
+			d[0].atributo = d[1].atributo;
+			d[0].simb = L;
+			d[0].estado = tabEstado[estado][L];
 			push(d[0]);
 			break;
 		case r9:
 			pop(3);
 			d[0].atributo = temp();
+			d[0].simb = E;
+			d[0].estado = d[1].estado;
 			gera(ADD, d[1].atributo, d[3].atributo, d[0].atributo);
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r10:
 			pop(1);
+			d[0].atributo = d[1].atributo;
+			d[0].simb = E;
+			d[0].estado = tabEstado[estado][E];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r11:
 			pop(3);
 			d[0].atributo = temp();
+			d[0].simb = T;
+			d[0].estado; = d[1].estado;
 			gera(MUL, d[1].atributo, d[3].atributo, d[0].atributo);
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r12:
 			pop(1);
+			d[0].atributo = d[1].atributo;
+			d[0].simb = T;
+			d[0].estado = tabEstado[estado][T];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r13:
 			pop(3);
 			d[0].atributo = d[2].atributo;
+			d[0].simb = F;
+			d[0].estado = tabEstado[estado][F];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r14:
 			pop(1);
-			d[0].atributo = incluiTabSimb(d[1].simbolo, Variavel);
+			d[0].atributo = incluiTabSimb(d[1].simb, Variavel);
+			d[0].simb = F;
+			d[0].estado = tabEstado[estado][F];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r15:
 			pop(1);
-			d[0].atributo = incluiTabSimb(d[1].symbol, Constante);
+			d[0].atributo = incluiTabSimb(d[1].simb, Constante);
+			d[0].simb = F;
+			d[0].estado = tabEstado[estado][F];
+			push(d[0]);
+			estado = d[0].estado;
 			break;
 		case r16:
-			pop(0);
+			// Vazio
 			break;
 		case r17:
-			pop();
+			// Vazio
 			break;
 	}
 }
