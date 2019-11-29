cf2d4d9c3b6e9fe008408574369c3a2a3f738731
Denilson Rodrigues <jundenilson@gmail.com>
2019-11-29 20:11:56 -0300

    dendengo

--- src/trata_reducao.c
+++ src/trata_reducao.c
@@ -1,3 +1,8 @@
+void _pop(int n ){
+	for(int ii = n; ii > 0; ii--){
+		unidPilhaDePop[ii] = pop()
+	}
+}
 void trataReducao()
 {
 	switch (tabEstado[estado][token])
@@ -6,30 +11,30 @@ void trataReducao()
 			encerra();
 			break;
 		case r1:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			// Ver se tem push
 			break;
 		case r2:
-			pop(3);
-			unidPilhaDePop[1].atributo = incluiTabSimb(unidPilhaDePop[1], Variavel);
+			_pop(3);
+			unidPilhaDePop[1].atributo.end = incluiTabSimb(unidPilhaDePop[1].atributo.asciiDoFont, Variavel);
 			unidPilhaDePop[0].estado = tabEstado[estado][E];
 			gera(STO, unidPilhaDePop[3].atributo, unidPilhaDePop[1].atributo);
 			break;
 		case r3:
 			// IF
-			pop(10);
+			_pop(10);
 			remenda(unidPilhaDePop[5].indiceQuadrupla, JF, unidPilhaDePop[3].indiceSimb, unidPilhaDePop[8].indiceQuadrupla+1, NADA);
 			remenda(unidPilhaDePop[8].indiceQuadrupla, J, prox, NADA, NADA);
 			break;
 		case r4:
 			// While
-			pop(8);
+			_pop(8);
 			gera (J, unidPilhaDePop[2].indiceQuadrupla, NADA, NADA);
 			remenda(unidPilhaDePop[4].indiceQuadr, JF, unidPilhaDePop[3].indiceSimb, prox, NADA);
 			break;
 		case r5:
-			pop(3);
+			_pop(3);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[2].atributo;
 			unidPilhaDePop[0].simb = B;
 			unidPilhaDePop[0].estado = tabEstado[estado][B];
@@ -37,29 +42,29 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r6:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			unidPilhaDePop[0].simb = B;
 			unidPilhaDePop[0].estado = tabEstado[estado][B];
 			push(unidPilhaDePop[0]);
 			break;
 		case r7:	
-			pop(3);
+			_pop(3);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			unidPilhaDePop[0].simb = L;
 			unidPilhaDePop[0].estado = tabEstado[estado][L];
 			push(unidPilhaDePop[0]);
 			break;
 		case r8:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			unidPilhaDePop[0].simb = L;
 			unidPilhaDePop[0].estado = tabEstado[estado][L];
 			push(unidPilhaDePop[0]);
 			break;
 		case r9:
-			pop(3);
-			unidPilhaDePop[0].atributo = temp();
+			_pop(3);
+			unidPilhaDePop[0].atributo.end = temp();
 			unidPilhaDePop[0].simb = E;
 			unidPilhaDePop[0].estado = unidPilhaDePop[1].estado;
 			gera(ADD, unidPilhaDePop[1].atributo, unidPilhaDePop[3].atributo, unidPilhaDePop[0].atributo);
@@ -67,7 +72,7 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r10:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			unidPilhaDePop[0].simb = E;
 			unidPilhaDePop[0].estado = tabEstado[estado][E];
@@ -75,8 +80,8 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r11:
-			pop(3);
-			unidPilhaDePop[0].atributo = temp();
+			_pop(3);
+			unidPilhaDePop[0].atributo.end = temp();
 			unidPilhaDePop[0].simb = T;
 			unidPilhaDePop[0].estado; = unidPilhaDePop[1].estado;
 			gera(MUL, unidPilhaDePop[1].atributo, unidPilhaDePop[3].atributo, unidPilhaDePop[0].atributo);
@@ -84,7 +89,7 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r12:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			unidPilhaDePop[0].simb = T;
 			unidPilhaDePop[0].estado = tabEstado[estado][T];
@@ -92,7 +97,7 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r13:
-			pop(3);
+			_pop(3);
 			unidPilhaDePop[0].atributo = unidPilhaDePop[2].atributo;
 			unidPilhaDePop[0].simb = F;
 			unidPilhaDePop[0].estado = tabEstado[estado][F];
@@ -100,7 +105,7 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r14:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = incluiTabSimb(unidPilhaDePop[1].simb, Variavel);
 			unidPilhaDePop[0].simb = F;
 			unidPilhaDePop[0].estado = tabEstado[estado][F];
@@ -108,7 +113,7 @@ void trataReducao()
 			estado = unidPilhaDePop[0].estado;
 			break;
 		case r15:
-			pop(1);
+			_pop(1);
 			unidPilhaDePop[0].atributo = incluiTabSimb(unidPilhaDePop[1].simb, Constante);
 			unidPilhaDePop[0].simb = F;
 			unidPilhaDePop[0].estado = tabEstado[estado][F];
