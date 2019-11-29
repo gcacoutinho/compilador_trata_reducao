281bdb5865a3e759f828d32d9a8abc0e4af41ce7
Bruno <31594181+brunocriscuolo@users.noreply.github.com>
2019-11-29 19:35:46 -0300

    Update trata_reducao.c

--- src/trata_reducao.c
+++ src/trata_reducao.c
@@ -1,5 +1,3 @@
-//trocar d[numeor] por unidPilhaDePop[MAXUNIDPILHA]
-//https://github.com/CarolCarvalhosa/pilha-compiladores-2019/blob/master/pilha.c
 void trataReducao()
 {
 	switch (tabEstado[estado][token])
@@ -9,113 +7,113 @@ void trataReducao()
 			break;
 		case r1:
 			pop(1);
-			d[0].atributo = d[1].atributo;
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
 			// Ver se tem push
 			break;
 		case r2:
 			pop(3);
-			d[1].atributo = incluiTabSimb(d[1], Variavel);
-			d[0].estado = tabEstado[estado][E];
-			gera(STO, d[3].atributo, d[1].atributo);
+			unidPilhaDePop[1].atributo = incluiTabSimb(unidPilhaDePop[1], Variavel);
+			unidPilhaDePop[0].estado = tabEstado[estado][E];
+			gera(STO, unidPilhaDePop[3].atributo, unidPilhaDePop[1].atributo);
 			break;
 		case r3:
 			// IF
 			pop(10);
-			remenda(d[5].indiceQuadrupla, JF, d[3].indiceSimb, d[8].indiceQuadrupla+1, NADA);
-			remenda(d[8].indiceQuadrupla, J, prox, NADA, NADA);
+			remenda(unidPilhaDePop[5].indiceQuadrupla, JF, unidPilhaDePop[3].indiceSimb, unidPilhaDePop[8].indiceQuadrupla+1, NADA);
+			remenda(unidPilhaDePop[8].indiceQuadrupla, J, prox, NADA, NADA);
 			break;
 		case r4:
 			// While
 			pop(8);
-			gera (J, d[2].indiceQuadrupla, NADA, NADA);
-			remenda(d[4].indiceQuadr, JF, d[3].indiceSimb, prox, NADA);
+			gera (J, unidPilhaDePop[2].indiceQuadrupla, NADA, NADA);
+			remenda(unidPilhaDePop[4].indiceQuadr, JF, unidPilhaDePop[3].indiceSimb, prox, NADA);
 			break;
 		case r5:
 			pop(3);
-			d[0].atributo = d[2].atributo;
-			d[0].simb = B;
-			d[0].estado = tabEstado[estado][B];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = unidPilhaDePop[2].atributo;
+			unidPilhaDePop[0].simb = B;
+			unidPilhaDePop[0].estado = tabEstado[estado][B];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r6:
 			pop(1);
-			d[0].atributo = d[1].atributo;
-			d[0].simb = B;
-			d[0].estado = tabEstado[estado][B];
-			push(d[0]);
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
+			unidPilhaDePop[0].simb = B;
+			unidPilhaDePop[0].estado = tabEstado[estado][B];
+			push(unidPilhaDePop[0]);
 			break;
 		case r7:	
 			pop(3);
-			d[0].atributo = d[1].atributo;
-			d[0].simb = L;
-			d[0].estado = tabEstado[estado][L];
-			push(d[0]);
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
+			unidPilhaDePop[0].simb = L;
+			unidPilhaDePop[0].estado = tabEstado[estado][L];
+			push(unidPilhaDePop[0]);
 			break;
 		case r8:
 			pop(1);
-			d[0].atributo = d[1].atributo;
-			d[0].simb = L;
-			d[0].estado = tabEstado[estado][L];
-			push(d[0]);
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
+			unidPilhaDePop[0].simb = L;
+			unidPilhaDePop[0].estado = tabEstado[estado][L];
+			push(unidPilhaDePop[0]);
 			break;
 		case r9:
 			pop(3);
-			d[0].atributo = temp();
-			d[0].simb = E;
-			d[0].estado = d[1].estado;
-			gera(ADD, d[1].atributo, d[3].atributo, d[0].atributo);
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = temp();
+			unidPilhaDePop[0].simb = E;
+			unidPilhaDePop[0].estado = unidPilhaDePop[1].estado;
+			gera(ADD, unidPilhaDePop[1].atributo, unidPilhaDePop[3].atributo, unidPilhaDePop[0].atributo);
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r10:
 			pop(1);
-			d[0].atributo = d[1].atributo;
-			d[0].simb = E;
-			d[0].estado = tabEstado[estado][E];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
+			unidPilhaDePop[0].simb = E;
+			unidPilhaDePop[0].estado = tabEstado[estado][E];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r11:
 			pop(3);
-			d[0].atributo = temp();
-			d[0].simb = T;
-			d[0].estado; = d[1].estado;
-			gera(MUL, d[1].atributo, d[3].atributo, d[0].atributo);
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = temp();
+			unidPilhaDePop[0].simb = T;
+			unidPilhaDePop[0].estado; = unidPilhaDePop[1].estado;
+			gera(MUL, unidPilhaDePop[1].atributo, unidPilhaDePop[3].atributo, unidPilhaDePop[0].atributo);
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r12:
 			pop(1);
-			d[0].atributo = d[1].atributo;
-			d[0].simb = T;
-			d[0].estado = tabEstado[estado][T];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = unidPilhaDePop[1].atributo;
+			unidPilhaDePop[0].simb = T;
+			unidPilhaDePop[0].estado = tabEstado[estado][T];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r13:
 			pop(3);
-			d[0].atributo = d[2].atributo;
-			d[0].simb = F;
-			d[0].estado = tabEstado[estado][F];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = unidPilhaDePop[2].atributo;
+			unidPilhaDePop[0].simb = F;
+			unidPilhaDePop[0].estado = tabEstado[estado][F];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r14:
 			pop(1);
-			d[0].atributo = incluiTabSimb(d[1].simb, Variavel);
-			d[0].simb = F;
-			d[0].estado = tabEstado[estado][F];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = incluiTabSimb(unidPilhaDePop[1].simb, Variavel);
+			unidPilhaDePop[0].simb = F;
+			unidPilhaDePop[0].estado = tabEstado[estado][F];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r15:
 			pop(1);
-			d[0].atributo = incluiTabSimb(d[1].simb, Constante);
-			d[0].simb = F;
-			d[0].estado = tabEstado[estado][F];
-			push(d[0]);
-			estado = d[0].estado;
+			unidPilhaDePop[0].atributo = incluiTabSimb(unidPilhaDePop[1].simb, Constante);
+			unidPilhaDePop[0].simb = F;
+			unidPilhaDePop[0].estado = tabEstado[estado][F];
+			push(unidPilhaDePop[0]);
+			estado = unidPilhaDePop[0].estado;
 			break;
 		case r16:
 			// Vazio
